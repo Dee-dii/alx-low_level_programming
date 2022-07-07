492ae1be9b275d3272e550aca4d9b3528a0a8ec3
Dee-dii <enividgaffy@gmail.com>
2022-07-06 21:56:38 -0700

    oops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /*
  * main - Entry point
@@ -8,15 +8,15 @@
 
 int main(void)
 {
-	int task_1[] = {95, 112, 117, 116, 99, 104, 97, 114}
-	int count, sz;
+	_putchar("_");
+	_putchar("p"); 
+	_putchar("u"); 
+	_putchar("t"); 
+	_putchar("c"); 
+	_putchar("h"); 
+	_putchar("a"); 
+	_putchar("r");
+        _putchar("\n");
 
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
-	{
-		_putchar(str[count]);
-	}
-	_putchar('\n');
-
-	return (0);
-}
+        return (0);
+}	
