6dfb403099aefdf3931bbc08d2b75381bd441df3
Dee-dii <enividgaffy@gmail.com>
2022-07-12 16:55:21 -0700

    put function

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts -> this is a function that puts
+ * @str: a param to _puts function
+ */
+void _puts(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[count]);
+		count++;
+	}
+}
+
