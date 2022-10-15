2f1ab426aa3246fc44ba2ed9231a572d183891d4
Dee-dii <enividgaffy@gmail.com>
2022-10-14 21:44:51 -0700

    0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints all base 10 single didits
+ *
+ * Return: Always returns 0 (success)
+ */
+int main(void)
+{
+	int i;
+	
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	return (0);
+}
