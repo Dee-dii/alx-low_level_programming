de10a94e0202a9f71f013170dcbce1c637631532
Dee-dii <enividgaffy@gmail.com>
2022-10-14 20:55:53 -0700

    small letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet in lowercase
+ *
+ * Return: Always returns 0 (success)
+ */
+int main(void)
+{
+	char aplhabets[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alphabets[i]);
+	}
+	putchar("\n");
+	return (0);
+}
