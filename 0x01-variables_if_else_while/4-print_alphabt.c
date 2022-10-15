4d1484db838d66a72442b688a868b7c282935af4
Dee-dii <enividgaffy@gmail.com>
2022-10-14 21:36:08 -0700

    missing two letters?

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet except q and e
+ *
+ * Return: Always returns (0)
+ */
+int main(void)
+{
+	char alp[24] = "abcdfghijklmnoprstuvwxyz";
+	int i;
+
+	for (i = 0; i < 24; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	result (0);
+}
