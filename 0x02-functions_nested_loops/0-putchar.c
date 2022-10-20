cefe58220230e2cf164f7b4eeeb499a845fdcd9f
Dee-dii <enividgaffy@gmail.com>
2022-10-19 21:25:49 -0700

    _putchar?

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,15 +7,16 @@
  */
 int main(void)
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-	_putchar("\n");
-	
+	int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
+	int count, len;
+
+	len = sizeof(str) / sizeof(int);
+
+	for (count = 0; count < len; count++)
+	{
+		_putchar(str[count]);
+	}
+
+	_putchar('\n');	
 	return (0);
 }
