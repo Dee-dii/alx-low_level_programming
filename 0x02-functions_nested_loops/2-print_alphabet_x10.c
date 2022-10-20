4561ba4b2b7b5f96e535056d8862c30c46864280
Dee-dii <enividgaffy@gmail.com>
2022-10-19 21:44:31 -0700

    multiplied strings

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -  prints 10 times the alphabet in lowercase
+ *
+ * Return: Always returns 0
+ */
+void print_alphabet_x10(void);
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (b = 97; b < 123; b++)
+		{
+			_putchar(b);
+		}
+	_putchar('\n');
+	}
+}
