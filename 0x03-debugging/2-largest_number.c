d51efd81e77d0c22ff010718ab05e6ff9f5b34b8
Dee-dii <enividgaffy@gmail.com>
2022-10-20 20:50:12 -0700

    Debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -26,22 +26,3 @@ int largest_number(int a, int b, int c)
 
 	return (largest);
 }
-
-/**
- * main - values container
- *
- * Return: largest number
- */
-int main(void)
-{
-	int a, b, c;
-	int largest;
-
-	a = 972;
-	b = -98;
-	c = 0;
-
-	largest = largest_number(a, b, c);
-
-	return (0);
-}
